0b12fe3568a42f8be6986af3e84786d62a4731bf
David <deh0_@hotmail.com>
2020-11-24 11:41:09 -0300

    alterado mensagem de sucesso, e erro ao realizar as operações de crud

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -52,11 +52,11 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
+                $this->Flash->success(__('Usuário cadastrado com sucesso.'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            $this->Flash->error(__('Erro ao cadastrar usuário, tente novamente!'));
         }
         $this->set(compact('user'));
     }
@@ -76,11 +76,11 @@ class UsersController extends AppController
         if ($this->request->is(['patch', 'post', 'put'])) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
+                $this->Flash->success(__('Usuário atualizado com sucesso.'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            $this->Flash->error(__('Erro ao atualizar usuário, tente novamente!'));
         }
         $this->set(compact('user'));
     }
@@ -97,9 +97,9 @@ class UsersController extends AppController
         $this->request->allowMethod(['post', 'delete']);
         $user = $this->Users->get($id);
         if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
+            $this->Flash->success(__('Usuário excluido com sucesso.'));
         } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+            $this->Flash->error(__('Erro ao cadastrar excluir usuário, tente novamente!'));
         }
 
         return $this->redirect(['action' => 'index']);
