587fdc26b5b7ffc01c4ca0f36ac876691e6ff1d2
David <deh0_@hotmail.com>
2020-11-24 11:36:08 -0300

    Criação do método para encriptar a senha no banco dedados

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -45,4 +47,14 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    /**
+     * Method for encrypt hash
+     */
+    protected function _setPassword($password)
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher)->hash($password);
+        }
+    }
 }
